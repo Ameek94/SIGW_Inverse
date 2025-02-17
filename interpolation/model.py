4d8689bb46da034eb4fbeeee63fee282a705eaff
Ameek94 <ameekmalhotra@gmail.com>
2025-02-17 13:50:53 +0000

    added aic to select optimal number of nodes

--- interpolation/model.py
+++ interpolation/model.py
@@ -20,10 +20,17 @@ from interpolation.omega_gw_jax import OmegaGWjax
 from scipy.optimize import basinhopping
 import math
 
-log10 = math.log(10.)
-
+# log10 = math.log(10.)
 
 ### start by defining some useful functions
+
+def pow10(x):
+    return jnp.power(10,x)
+
+def log10(x):
+    return jnp.log10(x)
+
+@jit
 def unit_transform(x,bounds):
     """
     Transform array from original domain given by bounds to [0,1]. 
@@ -31,7 +38,7 @@ def unit_transform(x,bounds):
     """
     ux = (x - bounds[0])/(bounds[1]-bounds[0])
     return ux
-
+@jit
 def unit_untransform(x,bounds):
     """
     Transform array to original domain from [0,1]
@@ -39,11 +46,12 @@ def unit_untransform(x,bounds):
     xu = x*(bounds[1]-bounds[0]) + bounds[0]
     return xu
 
+@jit
 def spline_predict_log(x_train,y_train,x_pred):
     """
-    Cubic spline to interpolate log P_zeta as a function of log_k
+    Cubic spline to interpolate log10 P_zeta as a function of log_k
     """
-    spl = CubicSpline(x_train,y_train,check=False,bc_type='natural')
+    spl = CubicSpline(x_train,y_train,check=False)
     y_pred = spl(x_pred)
     return y_pred
 
@@ -52,8 +60,8 @@ def spline_predict(x_train,y_train,x_pred):
     """
     Obtain spline prediction after exponentiating log P spline. 
     """
-    x_pred = jnp.log(x_pred)
-    return jnp.exp(spline_predict_log(x_train,y_train,x_pred))
+    x_pred = log10(x_pred)
+    return pow10(spline_predict_log(x_train,y_train,x_pred))
 
 def optim_scipy_bh(loss,x0,minimizer_kwargs={'method': 'L-BFGS-B'  },stepsize=1/4,niter=15,bounds=None):
 
@@ -117,7 +125,7 @@ def optim_optax(loss,x0,y_low,y_high,steps=100,start_learning_rate=1e-1, n_resta
 
     return best_params, best_val
 
-class interpolation_model:
+class Interpolation_Model:
     """
     Base class for interpolation, do not use directly.
     """
@@ -137,8 +145,11 @@ class interpolation_model:
         self.omgw_karr = omgw_karr
         self.omgw_means = omgw_means
         self.omgw_cov = omgw_cov
+        self.omgw_sigma = jnp.sqrt(jnp.diag(omgw_cov))
         self.omgw_invcov = jnp.linalg.inv(omgw_cov)
-        self.lnk_min, self.lnk_max = jnp.log(pz_kmin), jnp.log(pz_kmax)
+        self.omgw_len = len(omgw_means)
+        self.logfac = 0.5*self.omgw_len*jnp.log(2*math.pi) + 0.5*jnp.log(jnp.linalg.det(self.omgw_cov))
+        self.logk_min, self.logk_max = log10(pz_kmin), log10(pz_kmax)
         self.bounds = jnp.array([pz_kmin,pz_kmax])
         if omgw_method=='grid':
             self.omgw_func = OmegaGWGrid(omgw_karr=self.omgw_karr,)
@@ -158,7 +169,7 @@ class interpolation_model:
                           thinning=1,
                           jit_model_args=False,
                           seed=42):
-        kernel = NUTS(self.model,dense_mass=False,
+        kernel = NUTS(self.model,dense_mass=True,
                 max_tree_depth=6)
         mcmc = MCMC(kernel,num_warmup=num_warmup,
                 num_samples=num_samples,
@@ -168,11 +179,13 @@ class interpolation_model:
         seed=seed
         rng_key, _ = random.split(random.PRNGKey(seed), 2)
         mcmc.run(rng_key,extra_fields=("potential_energy",)) # this can be made faster - parallel
+        samples = mcmc.get_samples()
         extras = mcmc.get_extra_fields()
         mcmc.print_summary(exclude_deterministic=False)
-        return mcmc.get_samples(), extras
+        print("First few samples:", {k: v[:5] for k, v in samples.items()})
+        return samples, extras
     
-class fixed_nodes_model(interpolation_model):
+class Fixed_Nodes_Model(Interpolation_Model):
         
     def __init__(self,
                  nbins:int,
@@ -187,10 +200,10 @@ class fixed_nodes_model(interpolation_model):
                  y_high: float = 1.,
                  ):            
         super().__init__(nbins,pz_kmin,pz_kmax,omgw_karr,omgw_means,omgw_cov,omgw_method,omgw_method_kwargs=omgw_method_kwargs)
-        self.log_k_nodes = jnp.linspace(self.lnk_min,self.lnk_max,self.nbins) # fixed nodes of the interpolation
-        self.k = jnp.exp(self.log_k_nodes)
-        self.lny_low = y_low * log10
-        self.lny_high = y_high * log10
+        self.log_k_nodes = jnp.linspace(self.logk_min,self.logk_max,self.nbins) # fixed nodes of the interpolation
+        self.k_nodes = pow10(self.log_k_nodes)
+        self.logy_low = y_low
+        self.logy_high = y_high
         # print(self.omgw_karr.shape)
 
     def model(self,omgw_model_args=None,omgw_model_kwargs=None):
@@ -199,17 +212,17 @@ class fixed_nodes_model(interpolation_model):
         # omgw = self.omgw_func(pz_func = pz_interp) #,self.bounds,self.omgw_karr,*omgw_model_args,**omgw_model_kwargs)             numpyro.sample('omk',dist.MultivariateNormal(loc=self.omgw_means,covariance_matrix=self.omgw_cov),obs=omgw)
         train_y = self.sample_y()
         omgw = self.get_omgw_from_y(train_y)
-        # numpyro.sample('omk',dist.Normal(omks_mean,omks_sigma),obs=omks_gp) # this assumes omks are independent, if off-diagonal cov use below
-        numpyro.sample('omk',dist.MultivariateNormal(loc=self.omgw_means,covariance_matrix=self.omgw_cov),obs=omgw)
+        numpyro.sample('omk',dist.Normal(loc=self.omgw_means,scale = self.omgw_sigma),obs=omgw) # this assumes omks are independent, if off-diagonal cov use below
+        # numpyro.sample('omk',dist.MultivariateNormal(loc=self.omgw_means,covariance_matrix=self.omgw_cov),obs=omgw) # type: ignore
 
     def sample_y(self):
-        train_y = numpyro.sample('y',dist.Uniform(low=self.lny_low*jnp.ones(self.nbins),high=self.lny_high*jnp.ones(self.nbins))) 
+        train_y = numpyro.sample('y',dist.Uniform(low=self.logy_low*jnp.ones(self.nbins),high=self.logy_high*jnp.ones(self.nbins)))  # type: ignore
         return train_y
     
     def spline(self,y, x):
         pz = spline_predict(x_train=self.log_k_nodes,y_train=y,x_pred=x) #
-        pz = jnp.where(jnp.log(x)<self.lnk_min,0.,pz)
-        pz = jnp.where(jnp.log(x)>self.lnk_max,0.,pz)
+        pz = jnp.where(log10(x)<self.logk_min,0.,pz)
+        pz = jnp.where(log10(x)>self.logk_max,0.,pz)
         return pz
 
     def get_omgw_from_y(self,y):
@@ -220,12 +233,12 @@ class fixed_nodes_model(interpolation_model):
     def loss(self,y):
         omgw = self.get_omgw_from_y(y)
         domgw = omgw - self.omgw_means
-        return jnp.einsum("i,ij,j",domgw,self.omgw_invcov,domgw)
+        return 0.5*jnp.einsum("i,ij,j",domgw,self.omgw_invcov,domgw) + self.logfac
     
     # def run_optimiser(self, x0, steps=100, start_learning_rate=1e-1, n_restarts=4, jump_sdev=0.1):
         # return super().run_optimiser(self.loss, x0, self.y_low, self.y_high, steps, start_learning_rate, n_restarts, jump_sdev)
 
-class variable_nodes_model(fixed_nodes_model):
+class Variable_Nodes_Model(Fixed_Nodes_Model):
 
     def __init__(self,
                  nbins:int,
@@ -242,14 +255,14 @@ class variable_nodes_model(fixed_nodes_model):
         super().__init__(nbins,pz_kmin,pz_kmax,omgw_karr,omgw_means,omgw_cov,omgw_method,omgw_method_kwargs,y_low,y_high)
 
         self.bin_edges = jnp.linspace(0.01,0.99,nbins-1) 
-        self.bin_edges = unit_untransform(self.bin_edges,bounds=[self.lnk_min,self.lnk_max])
+        self.bin_edges = unit_untransform(self.bin_edges,bounds=[self.logk_min,self.logk_max])
         self.lows = self.bin_edges[:-1]
         self.highs = self.bin_edges[1:]
         print(self.lows,self.highs)
 
     def model(self):
         x_bins = numpyro.sample("x_bins",dist.Uniform(low=self.lows,high=self.highs))
-        x = numpyro.deterministic("x",jnp.concatenate([jnp.array([self.lnk_min]),jnp.array(x_bins),jnp.array([self.lnk_max])]) )
+        x = numpyro.deterministic("x",jnp.concatenate([jnp.array([self.logk_min]),jnp.array(x_bins),jnp.array([self.logk_max])]) )
         train_y = self.sample_y()
         omgw = self.get_omgw_from_xy(x,train_y)
         # numpyro.sample('omk',dist.Normal(omks_mean,omks_sigma),obs=omks_gp) # this assumes omks are independent, if off-diagonal cov use below
@@ -257,8 +270,8 @@ class variable_nodes_model(fixed_nodes_model):
 
     def spline(self,x, y, k):
         pz = spline_predict(x_train=x,y_train=y,x_pred=k) #
-        pz = jnp.where(jnp.log(k)<self.lnk_min,0.,pz)
-        pz = jnp.where(jnp.log(k)>self.lnk_max,0.,pz)
+        pz = jnp.where(log10(k)<self.logk_min,0.,pz)
+        pz = jnp.where(log10(k)>self.logk_max,0.,pz)
         return pz
 
     def get_omgw_from_xy(self,x,y):
@@ -268,7 +281,41 @@ class variable_nodes_model(fixed_nodes_model):
 
     def loss(self,params):
         x_bins, y = params[:self.nbins-2], params[self.nbins-2:]
-        x = jnp.concatenate([jnp.array([self.lnk_min]),jnp.array(x_bins),jnp.array([self.lnk_max])])
+        x = jnp.concatenate([jnp.array([self.logk_min]),jnp.array(x_bins),jnp.array([self.logk_max])])
         omgw = self.get_omgw_from_xy(x,y)
         domgw = omgw - self.omgw_means
-        return jnp.einsum("i,ij,j",domgw,self.omgw_invcov,domgw)
+        return 0.5*jnp.einsum("i,ij,j",domgw,self.omgw_invcov,domgw)
+    
+
+
+
+
+
+    #     def spline(self, x, y, k):
+    #     def compute_spline(x, y, k):
+    #         pz = spline_predict(x_train=x, y_train=y, x_pred=k)
+    #         pz = jnp.where(log10(k) < self.logk_min, 0., pz)
+    #         pz = jnp.where(log10(k) > self.logk_max, 0., pz)
+    #         return {'x': x, 'y': y, 'pz': pz}
+
+    #     self._cached_spline = cond(
+    #         self._cached_spline is None or not (jnp.array_equal(self._cached_spline['x'], x) and jnp.array_equal(self._cached_spline['y'], y)),
+    #         lambda _: compute_spline(x, y, k),
+    #         lambda _: self._cached_spline,
+    #         operand=None
+    #     )
+    #     return self._cached_spline['pz']
+
+    # def get_omgw_from_xy(self, x, y):
+    #     def compute_omgw(x, y):
+    #         pz_interp = lambda k: self.spline(x=x, y=y, k=k)
+    #         omgw = self.omgw_func(pz_interp, self.omgw_karr)
+    #         return {'x': x, 'y': y, 'omgw': omgw}
+
+    #     self._cached_omgw = cond(
+    #         self._cached_omgw is None or not (jnp.array_equal(self._cached_omgw['x'], x) and jnp.array_equal(self._cached_omgw['y'], y)),
+    #         lambda _: compute_omgw(x, y),
+    #         lambda _: self._cached_omgw,
+    #         operand=None
+    #     )
+    #     return self._cached_omgw['omgw']
--- interpolation/run.py
+++ interpolation/run.py
@@ -21,12 +21,10 @@ from interpolation.omega_gw_grid import OmegaGWGrid
 from interpolation.omega_gw_jax import OmegaGWjax
 from scipy.optimize import basinhopping
 import math
-
-from interpolation.model import interpolation_model, fixed_nodes_model, variable_nodes_model, optim_scipy_bh, optim_optax
+from interpolation.model import Interpolation_Model, Fixed_Nodes_Model, Variable_Nodes_Model, optim_scipy_bh, optim_optax
 
 
 class sampler:
-
 
     def __init__(self,                  
                 pz_kmin: float,
@@ -34,34 +32,38 @@ class sampler:
                 omgw_karr: jnp.ndarray,
                 omgw_means: jnp.ndarray,
                 omgw_cov: jnp.ndarray, 
-                omgw_method: str = 'grid',
+                omgw_method: str = 'jax',
                 omgw_method_kwargs: dict = {'s': jnp.linspace(0, 1, 15),   't': jnp.logspace(-4, 4, 200) },
+                y_low: float = -5,
+                y_high: float = 2,
                 interpolation_method: str = 'CubicSpline',
                 interpolation_model: str = 'FixedNodes',
-                min_nodes: int = 3,
+                min_nodes: int = 2,
                 max_nodes: int = 12,):
         
         self.omgw_karr = omgw_karr
         self.omgw_means = omgw_means
         self.omgw_cov = omgw_cov
         self.omgw_invcov = jnp.linalg.inv(omgw_cov)
+        self.pz_kmin, self.pz_kmax = pz_kmin, pz_kmax
         self.lnk_min, self.lnk_max = jnp.log(pz_kmin), jnp.log(pz_kmax)
         self.bounds = jnp.array([pz_kmin,pz_kmax])
-        if omgw_method=='grid':
-            self.omgw_func = OmegaGWGrid(omgw_karr=self.omgw_karr,)
-        elif omgw_method=='jax':
-            self.omgw_func = OmegaGWjax(f=self.omgw_karr,**omgw_method_kwargs)
-        else:
-            return ValueError("Not a valid method")
+        self.y_low = y_low
+        self.y_high = y_high
+        self.omgw_method_kwargs = omgw_method_kwargs
+        if omgw_method not in ['grid','jax']:
+            raise ValueError(f"{omgw_method} is not a valid method for calculating OmegaGW. Choose from ['grid','jax'].")
         self.min_nodes = min_nodes
         self.max_nodes = max_nodes
         self.interpolation_method = interpolation_method
         self.interpolation_model = interpolation_model
-        self.run_info = {"interpolation_method": self.interpolation_method, "interpolation_model": self.interpolation_model, ""}
+        self.run_info = {"interpolation_method": self.interpolation_method, "interpolation_model": self.interpolation_model}
         self.results_dict = {}
 
 
     def run(self):
+        for n in range(self.min_nodes,self.max_nodes+1):
+            self.optimise_model(n)        
         # for n in [min_nodes,max_nodes]
         #   optimize interpolation_model(n)
         #   results_dict_n = {"best_params": best_params, "chi2": chi2, "aic": aic}
@@ -69,8 +71,49 @@ class sampler:
         return self.results_dict
 
     def optimise_model(self,n):
-        results_dict_n = {}
-        return results_dict_n
+        nbins = n
+        pz_model = Fixed_Nodes_Model(nbins=nbins,pz_kmin=self.pz_kmin,pz_kmax=self.pz_kmax,
+                            omgw_karr=self.omgw_karr,omgw_means=self.omgw_means,
+                            omgw_cov=self.omgw_cov,omgw_method='jax',omgw_method_kwargs=self.omgw_method_kwargs
+                            ,y_low=self.y_low,y_high=self.y_high)
+        samples, extras = pz_model.run_hmc_inference(num_warmup=512,num_samples=512)
+        # # print(extras.keys())
+        best_idx = jnp.argmin(extras['potential_energy'])
+        x0 = samples['y'][best_idx]  #
+        # x0 = -0.5*jnp.ones(nbins) #np.random.uniform(low=pz_model.lnk_min,high=pz_model.lnk_max,size=nbins)
+        print(x0)
+        best_params, chi2 = optim_scipy_bh(x0 = x0,loss = pz_model.loss,bounds=(pz_model.logy_low,pz_model.logy_high)
+                                           ,stepsize=0.2,niter=6*nbins) 
+        # best_params, chi2 =  optim_optax(x0 = -1.5*jnp.ones(nbins), loss = pz_model.loss, start_learning_rate=0.3 , steps = 200, jump_sdev=1.,y_low=5.,y_high=2.)
+
+        aic = 2*nbins + chi2
+        print(f"Number of nodes: {n}, chi2: {chi2:.4f}, aic: {aic:.4f}, 'best_params': {best_params}")
+        results_dict_n = {'best_params': best_params, 'node_locations': pz_model.log_k_nodes, 'chi2': chi2, 'aic': aic}
+        # print(results_dict_n)
+        self.results_dict[str(n)] = results_dict_n
+        del pz_model, x0, best_params, chi2, aic, results_dict_n
+
+    # def plot_results(self,best_n=3):
+    #     fig,(ax1,ax2) = plt.subplots(1,2,figsize=(12,4))
+
+    #     ax1.loglog(p_arr,pz_bf(p_arr),color='r')
+    #     ax1.loglog(p_arr,pz_amp,color='k',lw=1.5)
+    #     ax2.plot(k_arr,omgw_amp,color='k',lw=1.5,label='Truth')
+    #     ax2.loglog(k_arr,omgw_bf,color='r',label='reconstructed')
+    #     ax2.fill_between(k_arr,omgw_amp+1.96*omks_sigma,omgw_amp-1.96*omks_sigma,alpha=0.2,color='C0')
+    #     ax2.set(yscale='log',xscale='log')
+    #     ax1.set_ylabel(r'$P_{\zeta}(k)$')
+    #     ax1.set_xlabel(r'$k$')
+    #     ax2.set_ylim(1e-4,1.)
+
+    #     ax2.set_ylabel(r'$\Omega_{\mathrm{GW}}(k)$')
+    #     ax2.set_xlabel(r'$k$')
+    #     ax2.legend()
+    #     for val in nodes:
+    #         ax1.axvline(jnp.exp(val),color='k',ls='-.',alpha=0.5)
+    #     ax1.scatter(jnp.exp(nodes),jnp.exp(best_params),color='r')
+    #     fig.tight_layout()
+    
 
         
 
